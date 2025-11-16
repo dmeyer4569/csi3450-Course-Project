131c20b5c9255bb224b0b87d5f2cab5fe3ad1c9a
dmeyer4569 <dmeyer2@oakland.edu>
2025-11-15 22:28:14 -0500

    created image cars association table, added first select for manufacturer and cars

--- backend/db/sqlscripts/select_db_scripts.py
+++ backend/db/sqlscripts/select_db_scripts.py
@@ -12,3 +12,16 @@ Probably will have the following :
 from sqlalchemy import text
 
 # select data from db 
+
+# cars by manufacturer
+get_manufact_cars = text(
+    """
+    SELECT c.carID, c.model, c.year, c.baseMSRP, c.manufacturerID, m.manufacturerName
+    FROM cars c
+    LEFT JOIN manufacturers m ON m.manufacturerID = c.manufacturerID
+    WHERE (:manufacturer_id IS NULL OR c.manufacturerID = :manufacturer_id)
+    AND (:manufacturer_name IS NULL OR m.manufacturerName LIKE '%' || :manufacturer_name || '%')
+    ORDER BY c.year DESC, c.model
+    LIMIT :limit OFFSET :offset;
+    """
+)
