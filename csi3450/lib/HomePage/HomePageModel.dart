d068524c1152df14785ffed2d2c5d9678514d3f2
itshaithamn <hnaksho@oakland.edu>
2025-11-19 18:01:22 -0500

    new commits

--- csi3450/lib/HomePage/HomePageModel.dart
+++ csi3450/lib/HomePage/HomePageModel.dart
@@ -1,7 +1,5 @@
-import '/components/car_card_widget.dart';
-import '/flutter_flow/flutter_flow_theme.dart';
-import '/flutter_flow/flutter_flow_util.dart';
-import '/flutter_flow/flutter_flow_widgets.dart';
+import './Components/CarCardModel.dart';
+import 'package:flutterflow_ui/flutterflow_ui.dart';
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
